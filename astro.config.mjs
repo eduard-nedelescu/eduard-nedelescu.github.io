0557cbcb1a7c0d0d520f1b23f1f90f1f93c3a766
George-Eduard NEDELESCU <149713913+eduard-nedelescu@users.noreply.github.com>
2026-08-14 02:20:54 +0300

    Add SEO foundation and site metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-    site: 'https://eduard-nedelescu.github.io'
+  site: 'https://eduard-nedelescu.github.io',
+  integrations: [sitemap()]
 });
